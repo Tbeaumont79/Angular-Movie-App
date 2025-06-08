78a2971c81952a69959476809566d86ef23ee495
tbeaumont79 <tbeaumont79@icloud.com>
2025-06-08 08:43:52 +0200

    feat: Navbar can be focus with the shortcut cmd + k

--- src/app/features/navbar/navbar.ts
+++ src/app/features/navbar/navbar.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   imports: [],
   templateUrl: './navbar.html',
 })
-export class Navbar {}
+export class Navbar {
+  @ViewChild('SearchInput') searchInput!: ElementRef<HTMLInputElement>;
+  @HostListener('document:keydown', ['$event'])
+  handleKeyBoardEvent(event: KeyboardEvent): void {
+    const isMac = navigator.userAgent.toLowerCase().includes('mac');
+    const isCmdK =
+      (isMac && event.metaKey && event.key === 'k') ||
+      (!isMac && event.ctrlKey && event.key === 'k');
+
+    if (isCmdK) {
+      event.preventDefault();
+      this.searchInput.nativeElement.focus();
+    }
+  }
+}
