421c35fd17a1644d8074895384dc36fc9b884c23
tbeaumont79 <tbeaumont79@icloud.com>
2025-06-08 18:15:24 +0200

    feat(home + interface): adding Movie type + apiUrl

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.html',
 })
-export class Home {}
+
+export class Home {
+  private readonly apiUrl : string = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+  private readonly apiKey : string = environment.apiKey;
+
+}
