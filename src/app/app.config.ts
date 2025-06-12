5497484ff909f7efea981b2ef1581724f707f559
tbeaumont79 <tbeaumont79@icloud.com>
2025-06-12 11:08:00 +0200

    feat: revert to relative paths for environment import

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@ import {
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { environment } from '@env/environment';
+import { environment } from '../environments/environment';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideImageKitLoader } from '@angular/common';
