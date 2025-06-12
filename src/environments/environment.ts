d2e943966107a366fd9a27de8681b40a2578bd64
tbeaumont79 <tbeaumont79@icloud.com>
2025-06-12 14:30:52 +0200

    revert: switch back to process.env with dot notation

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiKey: import.meta.env['VITE_apiKey'] || '',
-  imageKitId: import.meta.env['VITE_imageKitId'] || '',
+  apiKey: process.env.apiKey || '',
+  imageKitId: process.env.imageKitId || '',
 };
