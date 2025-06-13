aacf45d79d7ea6f19a1445a0f02925fe81f3fcb9
tbeaumont79 <tbeaumont79@icloud.com>
2025-06-13 15:48:08 +0200

    feat: adding a footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './features/navbar/navbar';
+import { Footer } from './features/footer/footer';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
 })
 export class App {}
